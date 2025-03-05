23eec791420a3b2e36243c70190300e2cd7ac673
GKDHARSHAN <152906237+GKDHARSHAN@users.noreply.github.com>
2025-03-05 20:27:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
 from flask import Flask
-from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
-from prometheus_client import CollectorRegistry
-from flask import Response
 
 app = Flask(__name__)
 
-# Define a Prometheus Counter metric
-REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests', ['method', 'endpoint'])
-
 @app.route('/')
-def hello():
-    REQUEST_COUNT.labels(method='GET', endpoint='/').inc()  # Increment the counter
-    return "Hello, World! Your Jenkins-Docker-Prometheus integration is successful!"
-
-@app.route('/metrics')
-def metrics():
-    registry = CollectorRegistry()
-    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+def home():
+    return "Hello, World! Your Jenkins-Docker integration is successful!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=80)

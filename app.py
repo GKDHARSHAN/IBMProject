578b673b47dc73b12f7a89a0b5cd37cb28624812
GKDHARSHAN <152906237+GKDHARSHAN@users.noreply.github.com>
2025-03-05 19:34:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return "Hello, World! Your Jenkins-Docker integration is successful!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8000)
